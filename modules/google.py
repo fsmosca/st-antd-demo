9db3a01233db69be7db02113f90226911a5c9f13
fsmosca <fsmosca@users.noreply.github.com>
2024-06-29 18:59:28 +0800

    Update google.py

--- modules/google.py
+++ modules/google.py
@@ -4,7 +4,23 @@ import streamlit as st
 def Android():
     st.header('Google/Android page')
 
+    st.markdown(
+        '''
+        **Google's Android is an open-source mobile operating system** used on various devices,
+        featuring a customizable user interface, extensive app ecosystem, and integration with
+        Google services like Gmail, Maps, and Play Store.
+        '''
+    )
+
 
 def Finance():
     st.header('Google/Finance page')
+
+    st.markdown(
+        '''
+        **Google Finance provides real-time** stock market quotes, financial news,
+        and portfolio tracking, offering users insights into global markets, investment trends,
+        and company performance through an intuitive, user-friendly interface.
+        '''
+    )
     
