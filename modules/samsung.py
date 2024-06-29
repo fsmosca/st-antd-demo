9a914f6b911f1cdaaf73195653b508ec45891084
fsmosca <fsmosca@users.noreply.github.com>
2024-06-29 18:59:31 +0800

    Update samsung.py

--- modules/samsung.py
+++ modules/samsung.py
@@ -3,3 +3,11 @@ import streamlit as st
 
 def Samsung():
     st.header('Samsung page')
+
+    st.markdown(
+        '''
+        **Samsung is a South Korean multinational conglomerate** known for its diverse range
+        of products and services, including consumer electronics, semiconductors, home appliances,
+        and telecommunications equipment. It's renowned for its innovative smartphones, TVs, and display technology.   
+        '''
+    )
