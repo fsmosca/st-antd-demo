cb07c29e35fc1006148dd11d14816d1bc7cd887c
fsmosca <fsmosca@users.noreply.github.com>
2024-06-29 18:59:26 +0800

    Update apple.py

--- modules/apple.py
+++ modules/apple.py
@@ -3,3 +3,10 @@ import streamlit as st
 
 def Apple():
     st.header('Apple page')
+
+    st.markdown(
+        '''
+        **Apple Inc. is a global technology** company known for its innovative products,
+        including the iPhone, iPad, Mac computers, Apple Watch, and software like iOS and macOS.
+        '''
+    )
