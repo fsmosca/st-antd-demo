8d5606ac29576b870b83e80ae05bedf4be36495e
fsmosca <fsmosca@users.noreply.github.com>
2024-06-30 10:38:14 +0800

    Add login, logout and register tabs in account

--- modules/account.py
+++ modules/account.py
@@ -3,4 +3,14 @@ import streamlit as st
 
 def Account():
     st.header('Account page')
-    
+
+    login, logout, register = st.tabs(['Login', 'Logout', 'Register'])
+
+    with login:
+        st.subheader('Login')
+
+    with logout:
+        st.subheader('Logout')
+
+    with register:
+        st.subheader('Register')
